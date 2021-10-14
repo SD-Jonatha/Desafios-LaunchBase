8386cd6c1e225e810557c3ff57987d93f1b191c5
Thiago Jonatha <58859350+SD-Jonatha@users.noreply.github.com>
2021-10-14 13:09:14 -0300

    Update desafio1-2.js

--- desafio1-2.js
+++ desafio1-2.js
@@ -1,19 +1,49 @@
-//Construção e impressão de objetos
-const empresa = {
-  nome: 'Rocketseat',
-  cor: 'Roxo',
-  Foco: 'Programacao',
-  Endereco: {
-    rua: 'Rua Guilherme Gembala',
-    Numero: 260
+// Desafios para fortalecer alguns conceitos, entre eles:
+
+// Objetos;
+// Vetores.
+// Construção e impressão de objetos
+// Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+
+// Nome: Rocketseat
+
+// Cor: Roxo
+
+// Foco: Programação
+
+// Endereço:
+
+// Rua: Rua Guilherme Gembala
+// Número: 260
+// Obs.: Para armazenar os dados de endereço da empresa você pode criar objetos intercalados, por exemplo:
+
+// const usuario = {
+//   nome: "Diego",
+//   empresa: {
+//     nome: "Rocketseat"
+//   }
+// };
+
+const usuario = {
+  nome: "Thiago",
+  empresa : {
+    nome: "Rocketseat",
+    Cor: "Roxo",
+    Foco: "Programação",
+    Endereco: {
+      Rua: "Rua Guilherme Gembala",
+      Numero: 260
+    }
   }
 }
 
-console.log(`A empresa ${empresa.nome} esta localizada em 
-${empresa.Endereco.rua}, ${empresa.Endereco.Numero} `)
+console.log(`A empresa ${usuario.empresa.nome} 
+está localizada em ${usuario.empresa.Endereco.Rua}, 
+${usuario.empresa.Endereco.Numero}`)
 
-//Vetores e objetos
-/* Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
+/*
+Vetores e objetos
+Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
 
 Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
 
@@ -21,18 +51,16 @@ As tecnologias também devem ser objetos contendo nome e especialidade, use as t
 
 { nome: 'C++', especialidade: 'Desktop' }
 { nome: 'Python', especialidade: 'Data Science' }
-{ nome: 'JavaScript', especialidade: 'Web/Mobile' } */
-
-const Programador = {
-  nome : 'Thiago',
-  idade : 28,
-  tecnologias : [
-    {
-      nome : 'Javascript',
-      especialidade : 'Web/Mobile'
-    }
-  ]  
+{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
+*/
+const usuario2 = {
+  nome: "Thiago",
+  idade: 30,
+  tecnologias: [
+    { nome: 'C++', especialidade: 'Desktop' },
+    { nome: 'Python', especialidade: 'Data Science' },
+    { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+  ]
 }
 
-console.log(`O usuario ${Programador.nome} tem ${Programador.idade} e usa
- a tecnologia ${Programador.tecnologias[0].nome} com especialidade em ${Programador.tecnologias[0].especialidade}`)
+console.log(`O usuário ${usuario2.nome} tem ${usuario2.idade} anos e usa a tecnologia ${usuario2.tecnologias[0].nome} com especialidade em ${usuario2.tecnologias[0].especialidade}`)
